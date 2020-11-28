51303a17c47417a534ab910be75c6f141f21b859
Phurba Sherpa <srpphurba@gmail.com>
2020-11-27 21:30:31 -0500

    Updates

---
+++ precache-manifest.223fab85e6aa2e472dabc0efc5b14ee1.js
@@ -0,0 +1,58 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1dd258a679f3c6282d505d6923822782",
+    "url": "/phrbshrp/index.html"
+  },
+  {
+    "revision": "9178a0435ea3d58830c5",
+    "url": "/phrbshrp/static/css/main.d343f35a.chunk.css"
+  },
+  {
+    "revision": "42f8718a43a1a706d548",
+    "url": "/phrbshrp/static/js/2.aa3d76b3.chunk.js"
+  },
+  {
+    "revision": "4ba1a1c11c57ed15aa68008707a3d6b4",
+    "url": "/phrbshrp/static/js/2.aa3d76b3.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9178a0435ea3d58830c5",
+    "url": "/phrbshrp/static/js/main.6e9e6558.chunk.js"
+  },
+  {
+    "revision": "4fffb37330dd8d9b1a55",
+    "url": "/phrbshrp/static/js/runtime-main.eec4f255.js"
+  },
+  {
+    "revision": "83f7033628f146dd96d7f8d91476a8cb",
+    "url": "/phrbshrp/static/media/Brand.83f70336.png"
+  },
+  {
+    "revision": "fcba68d631ed1e302b28d05b424b60a8",
+    "url": "/phrbshrp/static/media/CalsTracker.fcba68d6.png"
+  },
+  {
+    "revision": "f28409d6e6120ee822aee2e0d058cad7",
+    "url": "/phrbshrp/static/media/CalsTracker2.f28409d6.png"
+  },
+  {
+    "revision": "f4cf41c8d7647b2665ecce6ac966bf3e",
+    "url": "/phrbshrp/static/media/Eztextbook.f4cf41c8.png"
+  },
+  {
+    "revision": "d8e6017052e26d93a14b5d31a2c5cc51",
+    "url": "/phrbshrp/static/media/Stockapp.d8e60170.png"
+  },
+  {
+    "revision": "a57b299de89b23c3f77a4f799de4c794",
+    "url": "/phrbshrp/static/media/Visuo.a57b299d.png"
+  },
+  {
+    "revision": "c20e2262b7700d452b54f83e8d0d8e1b",
+    "url": "/phrbshrp/static/media/avatar.c20e2262.png"
+  },
+  {
+    "revision": "8c63bdb1752d479f603f51d520d9317b",
+    "url": "/phrbshrp/static/media/manComputer.8c63bdb1.png"
+  }
+]);
